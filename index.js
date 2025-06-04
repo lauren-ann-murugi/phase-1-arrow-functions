6c236472c2ab6e467b9d3d9a8468739368c2d0e8
laurenannmurugi <laurenann1871@gmail.com>
2025-06-04 22:49:34 +0300

    Complete lab

---
+++ index.js
@@ -0,0 +1,13 @@
+//create a function expression that divides two numbers
+const divide = function (a,b) {
+    return a / b;
+}
+//create an arrow function that squares a number
+const square = (x) => {
+    return x ** 2;
+}
+
+//create an arrow function that adds two numbers
+const add = (a, b) => {
+    return a + b;
+}
